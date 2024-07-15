2a4ddbc966b35957020cc40dc94006cd6bc6eed0
ryleigh <ryleighkostash@gmail.com>
2024-07-15 13:27:35 -0700

    ui tweaks

--- code/things/Player.cs
+++ code/things/Player.cs
@@ -266,8 +266,8 @@ public class Player : Thing
 	{
 		base.OnUpdate();
 
-		//Gizmo.Draw.Color = Color.White.WithAlpha(0.5f);
-		//Gizmo.Draw.Text( $"IsGameOver: {Manager.Instance.IsGameOver}", new global::Transform( (Vector3)Position2D + new Vector3( 0f, -0.7f, 0f ) ) );
+		//Gizmo.Draw.Color = Color.White.WithAlpha( 0.5f );
+		//Gizmo.Draw.Text( $"Health: {Health}", new global::Transform( (Vector3)Position2D + new Vector3( 0f, -0.7f, 0f ) ) );
 		
 		string debug = "";
 
