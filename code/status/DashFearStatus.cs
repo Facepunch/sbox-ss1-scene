dc222177786739f18445000374c981440986fdc9
ryleigh <ryleighkostash@gmail.com>
2024-07-15 13:05:57 -0700

    font

--- code/status/DashFearStatus.cs
+++ code/status/DashFearStatus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Sandbox;
 
-[Status( 1, 0, 111f )]
+[Status( 1, 0, 1f )]
 public class DashFearStatus : Status
 {
 	public DashFearStatus()
